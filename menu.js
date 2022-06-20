7e0ef488d5f0ee678845516289b773d493d12b51
wadaudzai <wadaudzai@gmail.com>
2022-06-21 04:06:14 +0430

    Update style.css and add menu.js

---
+++ menu.js
@@ -0,0 +1,7 @@
+const mobile_nav = document.querySelector(".menu");
+const nav_header = document.querySelector(".header");
+const toggleNavbar = () => {
+    nav_header.classList.toggle("active");
+};
+
+mobile_nav.addEventListener('click', () => toggleNavbar());
