a16b7175f6d558d76f2e655247782bbd59b6cb2f
wadaudzai <wadaudzai@gmail.com>
2022-06-21 19:31:55 +0430

    Fix linter errors

--- menu.js
+++ menu.js
@@ -1,7 +1,7 @@
-const mobile_nav = document.querySelector(".menu");
-const nav_header = document.querySelector(".header");
+const mobileNav = document.querySelector('.menu');
+const navHeader = document.querySelector('.header');
 const toggleNavbar = () => {
-    nav_header.classList.toggle("active");
+  navHeader.classList.toggle('active');
 };
 
-mobile_nav.addEventListener('click', () => toggleNavbar());
+mobileNav.addEventListener('click', () => toggleNavbar());
