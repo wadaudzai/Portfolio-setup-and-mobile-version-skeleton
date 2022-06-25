84913af8d8e05fe91c4fcc542c742711bd8577fd
wadaudzai <wadaudzai@gmail.com>
2022-06-25 14:58:47 +0430

    Add HTML, CSS, JS because I had some issues with the other branch I made it this way

--- menu.js
+++ menu.js
@@ -36,84 +36,86 @@ form.addEventListener('input', () => {
 inputName.value = localStorage.getItem('Name');
 email.value = localStorage.getItem('Email');
 textarea.value = localStorage.getItem('TextArea');
-const popuparray = [
+
+
+//Dynamics part
+
+let projectInfo = [
   {
-    heading: 'Tonic',
-    area: 'Canopy',
-    deveoper: 'Back End Dev',
-    date: '2015',
-    highlights:
-      " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    image: 'images/SnapshootPortfolio1.png',
-    btn1: 'html',
-    btn2: 'css',
-    btn3: 'javascript',
-    seeproject:
-      'https://basitali111.github.io/Portfolio-setup-and-mobile-version-skeleton/',
-    seesource:
-      'https://github.com/basitali111/Portfolio-setup-and-mobile-version-skeleton',
+    name: "Tonic",
+    title: "Tonic",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    "featured image": "./assets/images/tonic-page-1.png",
+    "featured image desk": "./assets/images/snaposhot-1-desktop-p.png",
+    technologies: ["html", "css", "javascript"],
+    "link to live version":
+      "https://wadaudzai.github.io/Portfolio-setup-and-mobile-version-skeleton/",
+    "link to source":
+      "https://github.com/wadaudzai/Portfolio-setup-and-mobile-version-skeleton",
+    id: 0, 
   },
   {
-    heading: 'Multi-Post Stories',
-    area: 'Canopy',
-    deveoper: 'Back End Dev',
-    date: '2015',
-    highlights:
-      "Lorem Ipsum is simply dummy text of the printing and typesettin industry. Lorem Ipsum has been the industry's standard dummy text eve since the 1500s, when an unknown printer took a galley of type andbled it to make a type specimen book. It has survived not onlyfive centuries, but also the leap into electronic typesetting, remaining essent",
-    image: 'images/SnapshootPortfolio.png',
-    btn1: 'html',
-    btn2: 'css',
-    btn3: 'javascript',
-    seeproject:
-      'https://basitali111.github.io/Portfolio-setup-and-mobile-version-skeleton/',
-    seesource:
-      'https://github.com/basitali111/Portfolio-setup-and-mobile-version-skeleton',
+    name: "Multi-Post Stories",
+    title: "Multi-Post Stories",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    "featured image": "./assets/images/Multi-post-stories.png",
+    "featured image desk": "./assets/images/multi-post-portfolio-desktop-1.svg",
+    technologies: ["html", "css", "javascript"],
+    "link to live version":
+      "https://wadaudzai.github.io/Portfolio-setup-and-mobile-version-skeleton/",
+    "link to source":
+      "https://github.com/wadaudzai/Portfolio-setup-and-mobile-version-skeleton",
+    id: 1,
   },
   {
-    heading: 'Tonic',
-    area: 'Canopy',
-    deveoper: 'Back End Dev',
-    date: '2015',
-    highlights:
-      " Lorem Ipsum is simply dummy text of the printing and typesettin industry. Lorem Ipsum has been the industry's standard dummy text eve since the 1500s, when an unknown printer took a galley of type andbled it to make a type specimen book. It has survived not onlyfive centuries, but also the leap into electronic typesetting, remaining essent",
-    image: 'images/SnapshootPortfolio(1).png',
-    btn1: 'html',
-    btn2: 'css',
-    btn3: 'javascript',
-    seeproject:
-      'https://basitali111.github.io/Portfolio-setup-and-mobile-version-skeleton/',
-    seesource:
-      'https://github.com/basitali111/Portfolio-setup-and-mobile-version-skeleton',
+    name: "Tonic",
+    title: "Facebook 360",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    "featured image": "./assets/images/Tonic-page3.png",
+    "featured image desk": "./assets/images/facebook-360-desktop.svg",
+    technologies: ["html", "css", "javascript"],
+    "link to live version":
+      "https://wadaudzai.github.io/Portfolio-setup-and-mobile-version-skeleton/",
+    "link to source":
+      "https://github.com/wadaudzai/Portfolio-setup-and-mobile-version-skeleton",
+    id: 2,
   },
   {
-    heading: 'Multi-Post Stories',
-    area: 'Canopy',
-    deveoper: 'Back End Dev',
-    date: '2015',
-    highlights:
-      "Lorem Ipsum is simply dummy text of the printing and typesettin industry. Lorem Ipsum has been the industry's standard dummy text eve since the 1500s, when an unknown printer took a galley of type andbled it to make a type specimen book. It has survived not onlyfive centuries, but also the leap into electronic typesetting, remaining essent",
-    image: 'images/SnapshootPortfolio(2).png',
-    btn1: 'html',
-    btn2: 'css',
-    btn3: 'javascript',
-    seeproject:
-      'https://basitali111.github.io/Portfolio-setup-and-mobile-version-skeleton/',
-    seesource:
-      'https://github.com/basitali111/Portfolio-setup-and-mobile-version-skeleton',
+    name: "Multi-Post Stories",
+    title: "Uber Navigation",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    "featured image": "./assets/images/multi-post-stories-2.png",
+    "featured image desk": "./assets/images/uber-nav-desktop.svg",
+    technologies: ["html", "css", "javascript"],
+    "link to live version":
+      "https://wadaudzai.github.io/Portfolio-setup-and-mobile-version-skeleton/",
+    "link to source":
+      "https://github.com/wadaudzai/Portfolio-setup-and-mobile-version-skeleton",
+    id: 3,
   },
 ];
 
-popuparray.forEach((element, i) => {
-  const popup = document.querySelector('.work');
+projectInfo.forEach((element, i) => {
+  const work = document.querySelector('.work');
   const card = document.createElement('article');
   card.classList = 'work-section';
- 
+  if (i === 1){
+    card.setAttribute('id', 'flipped');
+  } else if (i === 3) {
+    card.setAttribute('id', 'flipped');
+  }
+
   const popupcard = `
-  <img src="${popuparray[0].image}" alt="Snapshot Portfolio" class="portfolio-img-1-mobile">
-<img src="./assets/images/snaposhot-1-desktop-p.png" alt="Snapshot Portfolio"
+  <img src="${projectInfo[i]["featured image"]}" alt="Snapshot Portfolio" class="portfolio-img-1-mobile">
+<img src="${projectInfo[i]["featured image desk"]}" alt="Snapshot Portfolio"
     class="portfolio-img-1-desktop">
 <article class="work-section-texts">
-    <h2 class="project-title-heading">Tonic</h2>
+    <h2 class="project-title-heading text-mobile">${projectInfo[i].name}</h2>
+    <h2 class="project-title-heading text-desktop">${projectInfo[i].title}</h2>
 
     <article class="frame-2">
         <h2 class="client-heading text-mobile">CANOPY</h2>
@@ -135,11 +137,12 @@ popuparray.forEach((element, i) => {
 
     <ul class="logo-tags">
         <li><a href="#"><img src="./assets/icons/Htmllogo.png" alt="Htmllogo"></a></li>
+        <li><a href="#"><img src="./assets/icons/ruby-n-rails-tag.png" alt="RubyLogo" class="portfolio-img-1-desktop"></a></li>
         <li><a href="#"><img src="./assets/icons/csslogo.png" alt="csslogo"></a></li>
         <li><a href="#"><img src="./assets/icons/jslogo.png" alt="jslogo"></a></li>
     </ul>
     <div class="button-container">
-        <button class="button-see-project" type="button">
+        <button class="button-see-project" type="button" id="${projectInfo[i].id}">
             See project
         </button>
     </div>
@@ -147,7 +150,91 @@ popuparray.forEach((element, i) => {
 
 `;
   card.innerHTML += popupcard;
-  popup.appendChild(card);
+  work.appendChild(card);
 });
+
+//button
+const buttonsee = document.querySelectorAll(".button-see-project");
+buttonsee.forEach((btn) => {
+  btn.addEventListener("click", makeDiv);
+});
+
+//function is to create div and append it to body and add class to it
+function makeDiv(event) {
+  let popup = document.createElement("div");
+  document.body.append(popup);
+  popup.classList.add("pop-up-class");
+
+  //add elements
+  let header = document.createElement('div');
+  header.classList.add('header-pp')
+  let projecthead = document.createElement("h2");
+  projecthead.classList.add("project-head");
+  let projectimg = document.createElement("img");
+  projectimg.classList.add('image-pp')
+  let positiondiv = document.createElement('div');
+  positiondiv.classList.add('position-div-pp');
+  let projectdescrip = document.createElement("p");
+  projectdescrip.classList.add("project-descrip");
+  let langbtndiv = document.createElement('div');
+  langbtndiv.classList.add('lang-btn-div');
+  let projecttech = document.createElement("ul");
+  projecttech.classList.add('lang-container-pp');
+  let projectbtncontainer = document.createElement("div");
+  let btnseelive = document.createElement("a");
+  let btnseesource = document.createElement("a");
+  let closeButton = document.createElement("button");
+  closeButton.classList.add("closeButton-class");
+  closeButton.id = "close-button";
+
+  //append elements inside the div
+  popup.append(
+    header,
+    projecthead,
+    closeButton,
+    projectimg,
+    positiondiv,
+    projectdescrip,
+    langbtndiv,
+    projecttech,
+    projectbtncontainer
+  );
+  header.append(projecthead, closeButton);
+  positiondiv.append(projectdescrip, langbtndiv, projecttech, projectbtncontainer);
+  langbtndiv.append(projecttech,projectbtncontainer);
+  projectbtncontainer.append(btnseelive, btnseesource);
+
+  //add content to the elements of div
+  let projectId = parseInt(event.target.id, 10);
+  console.log(projectId);
+  projecthead.textContent = projectInfo[projectId].name;
+  projectimg.src = projectInfo[projectId]["featured image"];
+  projectdescrip.textContent = projectInfo[projectId]["description"];
+
+  for (let i = 0; i < projectInfo[projectId].technologies.length; i += 1) {
+    const projectLi = document.createElement('li');
+    projectLi.className = 'languages-pp';
+    projectLi.textContent = projectInfo[projectId].technologies[i];
+    projecttech.appendChild(projectLi);
+  }
+
+  projectbtncontainer.classList.add('btn-container-class-pp')
+  btnseelive.href = projectInfo[projectId]['link to live version']
+  btnseelive.classList.add('btn-seelive-pp')
+  btnseelive.target = '_blank'
+  btnseelive.textContent = 'See Live'
+  btnseesource.href = projectInfo[projectId]['link to source']
+  btnseesource.classList.add('btn-seesource-pp')
+  btnseesource.target = '_blank'
+  btnseesource.textContent = 'See Source'
+
+
+  //close button function
+  const clickCloseButton = document.getElementById("close-button");
+  clickCloseButton.addEventListener("click", closeButtonFunction);
+  function closeButtonFunction() {
+    popup.remove();
+  }
+}
 
 
